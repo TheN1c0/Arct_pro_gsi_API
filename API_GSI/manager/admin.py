07b5df0919e1d9686cdc1f4deb70d60984aa71f4
Nicolas Bravo <nicojjj4@gmail.com>
2024-10-10 09:48:47 -0300

    Se corrigió el erro de agregar productos, estaban mal los id que recibia y por eso no encontraba el producto

--- API_GSI/manager/admin.py
+++ API_GSI/manager/admin.py
@@ -3,3 +3,10 @@ from .models import Producto, Categoria
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Categoria)
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('idProducto', 'nombre', 'price', 'stock', 'categoria')
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('idCategoria', 'nombre', 'descripcion')
