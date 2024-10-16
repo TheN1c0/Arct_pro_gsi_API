6c7f827081bfa8c7f7eaa641ca4ad55b2c04afa8
Nicolas Bravo <nicojjj4@gmail.com>
2024-10-15 21:31:02 -0300

    Se agregó el modelo de pedido y detallePedido (no se a migrado)

--- API_GSI/manager/models.py
+++ API_GSI/manager/models.py
@@ -24,3 +24,31 @@ class Producto(models.Model):
     
     def __str__(self):
         return self.nombre
+
+from django.db import models
+
+class Pedido(models.Model):
+    nombre = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    estado = models.CharField(max_length=50)
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def calcular_monto_total(self):
+        # Recalcula el total de los detalles en el back-end
+        self.monto_total = sum(detalle.total for detalle in self.detalles.all())
+        self.save()
+
+    def __str__(self):
+        return f"Pedido {self.id} - {self.client_name}"
+
+class DetallePedido(models.Model):
+    pedido = models.ForeignKey(Pedido, related_name='detalles', on_delete=models.CASCADE)
+    product_id = models.IntegerField()
+    cantidad = models.PositiveIntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Detalle del Pedido {self.pedido.id} - Producto {self.product_id}"
